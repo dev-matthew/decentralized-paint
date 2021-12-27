43f0b1c5eee2bff8fa44c133b34caca74b59fc2e
Matthew <80741503+dev-matthew@users.noreply.github.com>
2021-12-27 01:20:47 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-/*const metamask_connect = document.getElementById("metamask_connect");
-
-metamask_connect.addEventListener("click", function() {
-    getMetamask();
-});
-
-async function getMetamask() {
-    const accounts = await ethereum.request({method: "eth_requestAccounts"});
-    const account = accounts[0];
-    const chain_id = await ethereum.request({method: "eth_chainId"});
-}*/
-
 window.addEventListener("load", function() {
     loadEmptyCanvas();
     addPixelListeners();
+
     if (typeof window.ethereum !== 'undefined') {
-        console.log("MetaMask installed");
         getMetamask();
+    } else {
+        setAccontStatus("Not Connected", "red");
     }
+
     populateCanvas();
 });
 
 async function getMetamask() {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    document.getElementById("account-status").innerHTML = account.substring(0,6) + "..." + account.slice(-4);
+    try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        setAccontStatus(account.substring(0,6) + "..." + account.slice(-4), "green");
+        setNetwork();
+    } catch(error) {
+        setAccontStatus("Not Connected", "red");
+    }
+}
+
+async function setNetwork() {
+    const networks = {
+        "0x1": "ETH Mainnet",
+        "0x89": "Polygon",
+        "0x3": "Ropsten",
+        "0x2a": "Kovan",
+        "0x4": "Rinkeby",
+        "0x5": "Goerli"
+    }
+
+    const chain_id = await ethereum.request({method: "eth_chainId"});
+    if (Object.keys(networks).includes(chain_id)) {
+        document.getElementById("network-status").innerHTML = networks[chain_id];
+    } else {
+        document.getElementById("network-status").innerHTML = chain_id;
+    }
+}
+
+function setAccontStatus(status, color) {
+    document.getElementById("account-status").innerHTML = status;
+        document.getElementById("account-status-icon").style.backgroundColor = color;
+        document.getElementById("account-status-icon").style.boxShadow = "0px 0px 10px 1px " + color;
 }
 
 function setStatus(status) {
@@ -49,7 +69,9 @@ function loadEmptyCanvas() {
     }
 }
 
-function populateCanvas() {
+async function populateCanvas() {
+    const web3 = new Web3("https://rinkeby.infura.io/v3/586a399ff09641fcab0bccc875b1ce1b");
+
     setStatus("Retrieved Canvas");
 }
 
